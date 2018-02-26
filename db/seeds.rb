5403ec1c1866b49d0390b3a1c542eacbb82461d1
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-26 19:10:05 +0000

    Colocando segundo nome nos usuários administradores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+
+puts 'Inserindo os administradores do sistema'
+admin_array = [
+    ['João Victor', 'joao@admin.com'],
+    ['Nicolas Rodrigues', 'nicolas@admin.com'],
+    ['Pedro Paiva', 'pedro@admin.com'],
+    ['Rui Braz', 'rui@admin.com']
+]
+
+admin_array.each do |nome, email| 
+    User.create!(email: email, name: nome, admin: true, approved: true, password: '123123', password_confirmation: '123123')
+end
