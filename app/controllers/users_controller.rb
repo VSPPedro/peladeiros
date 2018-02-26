cf71cce2b67dacee840e914f9f84630855ae7032
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-26 20:41:26 +0000

    Refatorando a página index de usuários para listar os administradores em uma tabela, os usuários aprovados em outra e os usuários pendentes em outra.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @admins = User.all.select{ |user| user.admin == true }.sort_by {|user| user.name}
+    @users = User.all.select{ |user| user.approved == true && user.admin != true }.sort_by {|user| user.name}
+    @pending_users = User.all.select{ |user| user.approved == false}.sort_by {|user| user.name}
   end
 
   def show
     @user = User.find(params[:id])
     unless current_user.admin || @user == current_user
-      redirect_to :back, :alert => "Acesso negado. Apenas os administradores podem ver os dados dos outros usuários."
+      redirect_to :back, :alert => "Acesso negado. Apenas os administradores podem ver os dados de outros usuários."
     end
   end
 
