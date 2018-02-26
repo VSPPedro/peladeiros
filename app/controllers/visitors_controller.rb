b43c84c6c791fecbb506fbae0285091ec8c547de
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-26 20:48:34 +0000

    Adicionando variável que contém a quantidade todos os usuários aprovados no sistema e outra que contém a quantidade todos os usuários pendentes.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,7 @@
 class VisitorsController < ApplicationController
+  
+  def index
+    @approved_users_count = User.all.select{ |user| user.approved == true }.count
+    @pending_users_count = User.all.select{ |user| user.approved == false }.count
+  end
 end
