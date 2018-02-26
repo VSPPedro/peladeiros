774c69af4db3d6597cd3b49f5d52504c6feec654
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-26 14:42:39 +0000

    Traduzindo aplicação para português

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsDevise
   class Application < Rails::Application
-
+  
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
@@ -23,5 +23,6 @@ module RailsDevise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :'pt-BR'
   end
 end
