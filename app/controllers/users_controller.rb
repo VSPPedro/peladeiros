ababac4250f758cfe0b838750e134ee5eb5cdeda
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-26 19:09:59 +0000

    Negando acesso a outros perfils de usuário para usuários que não sejam admin.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
+    unless current_user.admin || @user == current_user
+      redirect_to :back, :alert => "Acesso negado. Apenas os administradores podem ver os dados dos outros usuários."
     end
   end
 
