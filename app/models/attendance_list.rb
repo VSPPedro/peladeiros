006f040553cbbf4dfbfe14dca1e8b5ff8e87752a
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-28 17:03:34 +0000

    Modelando models many to many com users_attendance_list.

--- app/models/attendance_list.rb
+++ app/models/attendance_list.rb
@@ -1,3 +1,4 @@
 class AttendanceList < ApplicationRecord
-  has_many :user
+  has_many :user_attendance_list
+  has_many :user, through: :users_attendance_list
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  has_many :attendance_list
+  
+  has_many :user_attendance_list
+  has_many :attendance_list, through: :users_attendance_list
 end
