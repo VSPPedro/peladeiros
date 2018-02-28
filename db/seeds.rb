f64a11782004e8913d07e56d7c48283448f72089
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-28 01:08:40 +0000

    Adicionando Campus aos usários adicionados na seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,37 @@ admin_array = [
     ['Rui Braz', 'rui@admin.com']
 ]
 
+campus_array = [
+    'Areia',
+    'Cabedelo',
+    'Cabedelo Centro',
+    'Cajazeiras',
+    'Campina Grande',
+    'Catolé do Rocha',
+    'Esperança',
+    'Guarabira',
+    'Itabaiana',
+    'Itaporanga',
+    'João Pessoa',
+    'Mangabeira',
+    'Monteiro',
+    'Patos',
+    'Pedras de Fogo',
+    'Picuí',
+    'Princesa Isabel',
+    'Santa Luzia',
+    'Santa Rita',
+    'Soledade',
+    'Souza'
+]
+
 admin_array.each do |nome, email| 
     User.create!(email: email, name: nome, admin: true, approved: true, password: '123123', password_confirmation: '123123')
 end
 
 puts 'Inserindo usuários aprovados por administradores'
 54.times do
-    User.create!(email: Faker::Internet.email, name: Faker::Name.name, approved: true, password: '123123', password_confirmation: '123123')
+    User.create!(email: Faker::Internet.email, name: Faker::Name.name, approved: true, password: '123123', password_confirmation: '123123', campus: campus_array.sample)
 end
 
 puts 'Inserindo usuários não aprovados por administradores'
