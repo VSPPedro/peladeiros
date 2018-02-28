472d1e52a88aae0e50ba7094c3513ed9b35b60e6
Pedro Paiva <pedrovsp109@gmail.com>
2018-02-28 17:05:50 +0000

    Inserindo no Schema o model UsersAttendanceLists.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228014345) do
+ActiveRecord::Schema.define(version: 20180228165603) do
 
   create_table "attendance_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "openess"
@@ -43,4 +43,15 @@ ActiveRecord::Schema.define(version: 20180228014345) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "users_attendance_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "attendance_list_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["attendance_list_id"], name: "index_users_attendance_lists_on_attendance_list_id", using: :btree
+    t.index ["user_id"], name: "index_users_attendance_lists_on_user_id", using: :btree
+  end
+
+  add_foreign_key "users_attendance_lists", "attendance_lists"
+  add_foreign_key "users_attendance_lists", "users"
 end
